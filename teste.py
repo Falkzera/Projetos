10df84284aa3043a104707a2b93a6e7580929d14
Falkzera <falcovisk@gmail.com>
2024-07-09 12:46:30 -0300

    Teste

---
+++ teste.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        number = int(input('Digite um número: ').strip())
+        if number < 0:
+            print('Digite apenas números positivos.')
+            continue            
+    except:
+        print('Você não digitou um número.')
+        continue
+
+    fatorial = 1
+    conta = 1
+
+    while number >= fatorial:
+        conta *= fatorial
+    
+        fatorial += 1
+    print(f'O fatorial de {number} é {conta}')
+    break
